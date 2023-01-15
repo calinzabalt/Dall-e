5a82d37d75ad16877c584eedad945f863b873cf0
Mastercalin <calinzabalt@gmail.com>
2023-01-15 16:40:39 +0200

    dall-e api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+/* Material Ui */
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import CircularProgress from '@mui/material/CircularProgress';
+
+const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
+
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    // Get the images from local storage on first render
+    let storedImages = JSON.parse(localStorage.getItem('images')) || [];
+    setImages(storedImages);
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    fetch("https://api.openai.com/v1/images/generations", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${apiKey}`
+      },
+      body: JSON.stringify({
+        prompt: inputValue
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      let newImages = JSON.parse(localStorage.getItem('images')) || [];
+      newImages.push(data.data[0].url);
+      setImages(newImages);
+      setIsLoading(false);
+      localStorage.setItem('images', JSON.stringify(newImages));
+    });
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <h2>Create images with DALL-E</h2>
+        <form className='create_image' onSubmit={handleSubmit}>
+          <div className='item'>
+            <TextField
+              id="standard-multiline-static"
+              label="Create an image"
+              multiline
+              rows={4}
+              variant="standard"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              type="text"
+            />
+          </div>
+          <div className='item submit'>
+            <Button type="submit" variant="outlined">Create</Button>
+          </div>
+        </form>
+      </div>
+
+      <div className='result'>
+        {isLoading && 
+            <div className='loading'>
+              <CircularProgress />
+            </div>
+          }
+        {images && images.map((image, index) => {
+          return(
+            <div className='image' key={index}>
+                <a href={image} download='image.jpg' target="_blank" rel="noreferrer">
+                  <img src={image} alt='Generated'/>
+                  <Button variant="contained">Download</Button>
+                </a>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
-
 export default App;
